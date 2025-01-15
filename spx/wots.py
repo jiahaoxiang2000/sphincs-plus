118e278f133c62091a46144592f90e7e9f1e858c
Jiahao Xiang <jiahaoxiang2000@gmail.com>
2025-01-15 15:58:02 +0800

    refactor(wots): Simplify and optimize WOTS+ implementation
    - Remove unnecessary parameter definitions
    - Use Address class from spx.address module
    - Implement set_hash_addr and set_chain_addr methods in Address class
    - Simplify key generation and signing processes
    - Remove unused functions for signature verification

--- spx/wots.py
+++ spx/wots.py
@@ -1,47 +1,34 @@
 import hashlib
 from typing import List, Tuple
 
+from spx.address import Address
 from spx.utils import thash
+from spx.constant import *
 
 
-from .address import WOTSAddress
-
-# Parameters
-SPX_N = 16  # Hash output length in bytes
-SPX_WOTS_W = 16  # Winternitz parameter
-SPX_WOTS_LOGW = 4  # log2(SPX_WOTS_W)
-SPX_WOTS_LEN1 = 64  # Length of message part
-SPX_WOTS_LEN2 = 3  # Length of checksum part
-SPX_WOTS_LEN = SPX_WOTS_LEN1 + SPX_WOTS_LEN2  # Total length
-SPX_WOTS_BYTES = SPX_WOTS_LEN * SPX_N
-SPX_WOTS_PK_BYTES = SPX_WOTS_BYTES
-SPX_SHA256_OUTPUT_BYTES = 32
-SPX_SHA256_ADDR_BYTES = 22
-
-
-def prf_addr(key: bytes, addr: WOTSAddress) -> bytes:
+def prf_addr(key: bytes, addr: Address) -> bytes:
     """PRF function using SHA256"""
     addr_bytes = addr.to_bytes()[:SPX_SHA256_ADDR_BYTES]
     return hashlib.sha256(key + addr_bytes).digest()[:SPX_N]
 
 
 def gen_chain(
-    input_data: bytes, start: int, steps: int, pub_seed: bytes, addr: WOTSAddress
+    input_data: bytes, start: int, steps: int, pub_seed: bytes, addr: Address
 ) -> bytes:
     """Compute the chaining function"""
     out = input_data[:SPX_N]
 
     for i in range(start, min(start + steps, SPX_WOTS_W)):
-        addr.hash = i.to_bytes(4, "big")
+        addr.set_hash_addr(i)
         thash(out, out, 1, pub_seed, addr)
 
     return out
 
 
-def wots_gen_sk(sk_seed: bytes, addr: WOTSAddress) -> bytes:
+def wots_gen_sk(sk_seed: bytes, addr: Address) -> bytes:
     """Generate WOTS secret key element"""
     # no problem with this
-    addr.hash = bytes(4)  # Zero the hash address
+    addr.set_hash_addr(0)
     return bytearray(prf_addr(sk_seed, addr))
 
 
@@ -63,47 +50,14 @@ def base_w(msg: bytes, out_len: int) -> List[int]:
     return output
 
 
-def wots_gen_pk(sk_seed: bytes, pub_seed: bytes, addr: WOTSAddress) -> bytes:
+def wots_gen_pk(sk_seed: bytes, pub_seed: bytes, addr: Address) -> bytes:
     """Generate WOTS public key"""
     pk = bytearray()
 
     for i in range(SPX_WOTS_LEN):
-        addr.chain = i.to_bytes(4, "big")
+        addr.set_chain_addr(i)
         sk = wots_gen_sk(sk_seed, addr)
-        # TODO: gen_chain have error
         pk_element = gen_chain(sk, 0, SPX_WOTS_W - 1, pub_seed, addr)
-        pk.extend(pk_element)
-
-    return bytes(pk)
-
-
-def wots_sign(msg: bytes, sk_seed: bytes, pub_seed: bytes, addr: WOTSAddress) -> bytes:
-    """Generate WOTS signature"""
-    lengths = chain_lengths(msg)
-    sig = bytearray()
-
-    for i in range(SPX_WOTS_LEN):
-        addr.chain = i.to_bytes(4, "big")
-        sk = wots_gen_sk(sk_seed, addr)
-        sig_element = gen_chain(sk, 0, lengths[i], pub_seed, addr)
-        sig.extend(sig_element)
-
-    return bytes(sig)
-
-
-def wots_pk_from_sig(
-    sig: bytes, msg: bytes, pub_seed: bytes, addr: WOTSAddress
-) -> bytes:
-    """Compute public key from signature"""
-    lengths = chain_lengths(msg)
-    pk = bytearray()
-
-    for i in range(SPX_WOTS_LEN):
-        addr.chain = i.to_bytes(4, "big")
-        sig_element = sig[i * SPX_N : (i + 1) * SPX_N]
-        pk_element = gen_chain(
-            sig_element, lengths[i], SPX_WOTS_W - 1 - lengths[i], pub_seed, addr
-        )
         pk.extend(pk_element)
 
     return bytes(pk)
